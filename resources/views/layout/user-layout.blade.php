2a5b43d7d8d439fedfd79f87f34447fbc5fa7272
Kheuzayni <bayecheikhfall249@gmail.com>
2024-04-22 12:43:08 +0000

    change the link href to use laravel blade format

--- resources/views/layout/user-layout.blade.php
+++ resources/views/layout/user-layout.blade.php
@@ -26,8 +26,11 @@
   <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
 
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+  <!-- Template Main CSS File 
+    <link href="assets/css/style.css" rel="stylesheet">
+  -->
+  <!-- change the link href to use laravel blade format -->
+  <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: NiceAdmin

adece5aaa69f76437be4577911bb9a08cd6887db
Kheuzayni <bayecheikhfall249@gmail.com>
2024-04-22 16:22:49 +0000

    registration : controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,3 +11,4 @@ Route::get('/', function () {
 Route::get('/home', [ UserController::class, 'homePage']); 
 Route::get('/contact/page', [ UserController::class, 'ContactPage']); 
 Route::get('/registration/form', [ AuthController::class, 'loadRegisterForm']); 
+Route::post('/register/user', [ AuthController::class, 'registerUser']) -> name('registerUser'); 

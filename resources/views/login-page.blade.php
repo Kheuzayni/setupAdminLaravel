4ee27753a85924f99620ed43634a9cc89bd8eef3
Kheuzayni <bayecheikhfall249@gmail.com>
2024-04-25 12:15:32 +0000

    forget password

--- resources/views/login-page.blade.php
+++ resources/views/login-page.blade.php
@@ -9,30 +9,29 @@
   <meta content="" name="description">
   <meta content="" name="keywords">
 
-  <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+ <!-- Favicons -->
+ <link href="{{ asset('img/favicon.png')}}" rel="icon">
+ <link href="{{ asset('img/apple-touch-icon.png')}}" rel="apple-touch-icon">
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-  <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
-  <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
-  <link href="{{ asset('assets/vendor/quill/quill.snow.css')}}" rel="stylesheet">
-  <link href="{{ asset('assets/vendor/quill/quill.bubble.css')}}" rel="stylesheet">
-  <link href="{{ asset('assets/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
-  <link href="{{ asset('assets/vendor/simple-datatables/style.css')}}" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="{{ asset('assets/css/style.css" rel="stylesheet')}}">
+ <!-- Vendor CSS Files -->
+ <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+ <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+ <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+ <link href="{{ asset('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+ <link href="{{ asset('assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+ <link href="{{ asset('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+ <link href="{{ asset('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+ <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
+ <script src="{{ asset('js/jquery.min.js') }}"></script>
+ <script src="{{ asset('js/export-excel.min.js') }}"></script>
+ <script src="{{ asset('row_merger/dist/row-merge-bundle.min.js') }}"></script>
+ <!-- Template Main CSS File -->
+ <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: NiceAdmin
+  * Updated: Sep 18 2023 with Bootstrap v5.3.2
   * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Updated: Apr 20 2024 with Bootstrap v5.3.3
   * Author: BootstrapMade.com
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
@@ -63,37 +62,30 @@
                     <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
                     <p class="text-center small">Enter your username & password to login</p>
                   </div>
-
-                  <!-- Flash message -->
-                  @if(Session::has('success'))
-                    <div class="alert alert-success/">{{Session::get('Success')}}</div>
-                  @endif
-                  @if(Session::has('error'))
-                    <div class="alert alert-success/">{{Session::get('error')}}</div>
-                  @endif
-
-                  <form class="row g-3" action= "{{ route('LoginUser') }}" method= "POST" >
-                  @csrf
+                    {{-- display flash message here --}}
+                    @if (Session::has('success'))
+                    <div class="alert alert-success">{{Session::get('success')}}</div>
+                    @endif
+                    @if (Session::has('error'))
+                    <div class="alert alert-danger">{{Session::get('error')}}</div>
+                    @endif
+                  <form class="row g-3" action="{{ route('LoginUser')}}" method="POST">
+                    @csrf
                     <div class="col-12">
                       <label for="yourUsername" class="form-label">Username</label>
                       <div class="input-group has-validation">
-                        <span class="input-group-text" id="inputGroupPrepend">@</span>
-                        <input type="text" name="username" class="form-control" >
-                        <span class = "text-danger"> @error('username') {{$message}} @endError </span>                        
+                        <input type="text" name="username" class="form-control" value="{{old('username')}}">
                       </div>
+                      <span class="text-danger">@error('username'){{$message}}@enderror</span>
                     </div>
 
                     <div class="col-12">
                       <label for="yourPassword" class="form-label">Password</label>
-                      <input type="password" name="password" class="form-control" >
-                      <span class = "text-danger"> @error('password') {{$message}} @endError </span>
+                      <input type="password" name="password" class="form-control">
+                      <span class="text-danger">@error('password'){{$message}}@enderror</span>
                     </div>
-
                     <div class="col-12">
-                      <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="remember" value="true" id="rememberMe">
-                        <label class="form-check-label" for="rememberMe">Remember me</label>
-                      </div>
+                      <p class="small mb-0">Forgot password? <a href="/forgot/password">Recover here!</a></p>
                     </div>
                     <div class="col-12">
                       <button class="btn btn-primary w-100" type="submit">Login</button>
@@ -105,6 +97,15 @@
 
                 </div>
               </div>
+
+              <div class="credits">
+                <!-- All the links in the footer should remain intact. -->
+                <!-- You can delete the links only if you purchased the pro version. -->
+                <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+              </div>
+
             </div>
           </div>
         </div>
@@ -115,19 +116,20 @@
   </main><!-- End #main -->
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Vendor JS Files -->
-  <script src="{{ asset('assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
-  <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{ asset('assets/vendor/chart.js/chart.umd.js')}}"></script>
-  <script src="{{ asset('assets/vendor/echarts/echarts.min.js')}}"></script>
-  <script src="{{ asset('assets/vendor/quill/quill.js')}}"></script>
-  <script src="{{ asset('assets/vendor/simple-datatables/simple-datatables.js')}}"></script>
-  <script src="{{ asset('assets/vendor/tinymce/tinymce.min.js')}}"></script>
-  <script src="{{ asset('assets/vendor/php-email-form/validate.js')}}"></script>
-
-  <!-- Template Main JS File -->
-  <script src="{{ asset('assets/js/main.js')}}"></script>
+{{-- change the links for css and scripts to point to assets in public folder --}}
+   <!-- Vendor JS Files -->
+   <script src="{{ asset('assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+   <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+   <script src="{{ asset('assets/vendor/chart.js/chart.umd.js') }}"></script>
+   <script src="{{ asset('assets/vendor/echarts/echarts.min.js') }}"></script>
+   <script src="{{ asset('assets/vendor/quill/quill.min.js') }}"></script>
+   <script src="{{ asset('assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+   <script src="{{ asset('assets/vendor/tinymce/tinymce.min.js') }}"></script>
+   <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
+ 
+   <!-- Template Main JS File -->
+   <script src="{{ asset('assets/js/main.js') }}"></script>
+   <script src="{{ asset('js/jquery.min.js') }}"></script>
 
 </body>
 
--- resources/views/register-form.blade.php
+++ resources/views/register-form.blade.php
@@ -9,30 +9,29 @@
   <meta content="" name="description">
   <meta content="" name="keywords">
 
-   <!-- Favicons -->
-   <link href="{{ asset('assets/img/favicon.png')}}" rel="icon">
-  <link href="{{ asset('assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <!-- Favicons -->
+  <link href="{{ asset('img/favicon.png')}}" rel="icon">
+  <link href="{{ asset('img/apple-touch-icon.png')}}" rel="apple-touch-icon">
 
   <!-- Vendor CSS Files -->
-  <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
   <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
-  <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
-  <link href="{{ asset('assets/vendor/quill/quill.snow.css')}}" rel="stylesheet">
-  <link href="{{ asset('assets/vendor/quill/quill.bubble.css')}}" rel="stylesheet">
-  <link href="{{ asset('assets/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
-  <link href="{{ asset('assets/vendor/simple-datatables/style.css')}}" rel="stylesheet">
-
+  <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
+  <script src="{{ asset('js/jquery.min.js') }}"></script>
+  <script src="{{ asset('js/export-excel.min.js') }}"></script>
+  <script src="{{ asset('row_merger/dist/row-merge-bundle.min.js') }}"></script>
   <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: NiceAdmin
+  * Updated: Sep 18 2023 with Bootstrap v5.3.2
   * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Updated: Apr 20 2024 with Bootstrap v5.3.3
   * Author: BootstrapMade.com
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
@@ -63,56 +62,48 @@
                     <h5 class="card-title text-center pb-0 fs-4">Create an Account</h5>
                     <p class="text-center small">Enter your personal details to create account</p>
                   </div>
-                  <!-- Flash message -->
-                  @if(Session::has('success'))
-                    <div class="alert alert-success/">{{Session::get('Success')}}</div>
+                  {{-- display flash message here --}}
+                  @if (Session::has('success'))
+                      <div class="alert alert-success">{{Session::get('success')}}</div>
                   @endif
-                  @if(Session::has('error'))
-                    <div class="alert alert-success/">{{Session::get('error')}}</div>
+                  @if (Session::has('error'))
+                      <div class="alert alert-danger">{{Session::get('error')}}</div>
                   @endif
-
                   <form class="row g-3" action="{{ route('registerUser')}}" method="POST">
-                  @csrf
+                    @csrf
                     <div class="col-12">
                       <label for="yourName" class="form-label">Your Name</label>
                       <input type="text" name="name" class="form-control">
-                      <span class = "text-danger"> @error('name') {{$message}} @endError </span>
+                      <span class="text-danger">@error('name'){{$message}}@enderror</span>
                     </div>
 
                     <div class="col-12">
                       <label for="yourEmail" class="form-label">Your Email</label>
                       <input type="email" name="email" class="form-control">
-                      <span class = "text-danger"> @error('email') {{$message}} @endError </span>
+                      <span class="text-danger">@error('email'){{$message}}@enderror</span>
                     </div>
 
                     <div class="col-12">
                       <label for="yourUsername" class="form-label">Username</label>
                       <div class="input-group has-validation">
                         <span class="input-group-text" id="inputGroupPrepend">@</span>
-                        <input type="text" name="username" class="form-control" >
-                        <span class = "text-danger"> @error('username') {{$message}} @endError </span>
+                        <input type="text" name="username" class="form-control">
+                      </div>
+                      <span class="text-danger">@error('username'){{$message}}@enderror</span>
                     </div>
 
                     <div class="col-12">
                       <label for="yourPassword" class="form-label">Password</label>
                       <input type="password" name="password" class="form-control">
-                      <span class = "text-danger"> @error('password') {{$message}} @endError </span>
+                      <span class="text-danger">@error('password'){{$message}}@enderror</span>
                     </div>
-
                     <div class="col-12">
-                      <label for="yourPassword" class="form-label">Confirme Password</label>
-                      <input type="password" name="password_confirmation" class="form-control">
-                      <span class = "text-danger"> @error('password_confirmation') {{$message}} @endError </span>
-                    </div>
-
-                    <!-- <div class="col-12">
-                      <div class="form-check">
-                        <input class="form-check-input" name="terms" type="checkbox" value="" id="acceptTerms" required>
-                        <label class="form-check-label" for="acceptTerms">I agree and accept the <a href="#">terms and conditions</a></label>
-                        <div class="invalid-feedback">You must agree before submitting.</div>
+                        <label for="yourPassword" class="form-label">Confirm Password</label>
+                        <input type="password" name="password_confirmation" class="form-control">
+                        <span class="text-danger">@error('password_confirmation'){{$message}}@enderror</span>
                       </div>
-                    </div> -->
-                    <br/>
+
+                
                     <div class="col-12">
                       <button class="btn btn-primary w-100" type="submit">Create Account</button>
                     </div>
@@ -129,7 +120,7 @@
                 <!-- You can delete the links only if you purchased the pro version. -->
                 <!-- Licensing information: https://bootstrapmade.com/license/ -->
                 <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-                
+                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
               </div>
 
             </div>
@@ -144,17 +135,18 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="{{ asset('assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
-  <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{ asset('assets/vendor/chart.js/chart.umd.js')}}"></script>
-  <script src="{{ asset('assets/vendor/echarts/echarts.min.js')}}"></script>
-  <script src="{{ asset('assets/vendor/quill/quill.js')}}"></script>
-  <script src="{{ asset('assets/vendor/simple-datatables/simple-datatables.js')}}"></script>
-  <script src="{{ asset('assets/vendor/tinymce/tinymce.min.js')}}"></script>
-  <script src="{{ asset('assets/vendor/php-email-form/validate.js')}}"></script>
+  <script src="{{ asset('assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+  <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('assets/vendor/chart.js/chart.umd.js') }}"></script>
+  <script src="{{ asset('assets/vendor/echarts/echarts.min.js') }}"></script>
+  <script src="{{ asset('assets/vendor/quill/quill.min.js') }}"></script>
+  <script src="{{ asset('assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+  <script src="{{ asset('assets/vendor/tinymce/tinymce.min.js') }}"></script>
+  <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
 
   <!-- Template Main JS File -->
-  <script src="{{ asset('assets/js/main.js')}}"></script>
+  <script src="{{ asset('assets/js/main.js') }}"></script>
+  <script src="{{ asset('js/jquery.min.js') }}"></script>
 
 </body>
 
